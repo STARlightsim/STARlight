5978d9c1eaee47ae54f914c8a95cb942cc82dc73
odjuvsla <odjuvsla>
2013-09-05 20:08:22 +0000

    Adding pythia 8 interface for decaying particles
    Used for gamma-gamma to single meson

---
+++ include/pythiadecayer.h
@@ -0,0 +1,44 @@
+#ifndef PYTHIADECAYER_H
+#define PYTHIADECAYER_H
+
+#include "Pythia.h"
+#include "starlightparticle.h"
+#include "upcevent.h"
+
+class pythiaDecayer
+{
+
+public:
+
+    // Default constructor
+    pythiaDecayer();
+
+    // Copy constructor
+//     pythiaDecayer(const pythiaDecayer &obj);
+
+    // Destructor
+    virtual ~pythiaDecayer();
+
+    // Assignment operator
+//     pythiaDecayer& operator=(const pythiaDecayer& other);
+
+    // Initialise
+    void init();
+    
+    // Add particle to current event
+    void addParticle(const starlightParticle &p);
+    
+    // Execute event and return starlight type event
+    upcEvent execute();
+
+
+private:
+
+    Pythia8::Pythia _pythia;
+    
+    // Prohibited
+    pythiaDecayer(Pythia8::Pythia pythia);
+    bool operator==(const pythiaDecayer &other) const;
+
+};
+#endif // PYTHIADECAYER_H
