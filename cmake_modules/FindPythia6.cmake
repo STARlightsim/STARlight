d7ebc31cc80783114e1529b9dbeec7c9e2e8ec73
odjuvsla <odjuvsla>
2013-10-06 14:18:04 +0000

    Updated cmake file for finding pythia6
    There was a problem with capitalization of the filename.

--- cmake_modules/FindPythia6.cmake
+++ cmake_modules/FindPythia6.cmake
@@ -1,5 +1,6 @@
 
-FIND_LIBRARY(PYTHIA6_LIBRARY NAMES pythia6 PATHS /usr/lib /usr/lib64 /usr/local/lib $ENV{PYTHIADIR})
+MESSAGE(STATUS "$PYTHIADIR: $ENV{PYTHIADIR}")
+FIND_LIBRARY(PYTHIA6_LIBRARY NAMES Pythia6 PATHS $ENV{PYTHIADIR} /usr/lib /usr/lib64 /usr/local/lib )
 
 IF (PYTHIA6_LIBRARY)
    SET(PYTHIA6_FOUND TRUE)
@@ -11,10 +12,10 @@ IF (PYTHIA6_FOUND)
    ENDIF (NOT Pythia6_FIND_QUIETLY)
 ELSE (PYTHIA6_FOUND)
    IF (Pythia6_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find Pythia6. We search first in the normal library paths, then in $PYTHIADIR")
+      MESSAGE(FATAL_ERROR "Could not find Pythia6. We search first in $PYTHIADIR, then in the normal library paths.")
    ELSE(Pythia6_FIND_REQUIRED)
       IF(NOT Pythia6_FIND_QUIETLY)
-	 MESSAGE(STATUS "Could not find Pythia6. We search first in the normal library paths, then in $PYTHIADIR")
+         MESSAGE(STATUS "Could not find Pythia6. We search first in $PYTHIADIR, then in the normal library paths.")
       ENDIF(NOT Pythia6_FIND_QUIETLY)
    ENDIF (Pythia6_FIND_REQUIRED)
    
